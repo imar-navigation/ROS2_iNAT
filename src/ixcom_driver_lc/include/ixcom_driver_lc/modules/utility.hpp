1378161dc7bebccf6f2d50fd493360de1736b832
Paul <126870332+PaulZiMAR@users.noreply.github.com>
2024-09-12 15:22:48 +0200

    Delete src directory

--- src/ixcom_driver_lc/include/ixcom_driver_lc/modules/utility.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <rclcpp/rclcpp.hpp>
-#include <ixcom/ixcom.h>
-
-rclcpp::Time UpdateGPSTime(const XCOMHeader& header, int32_t leap_seconds);
-
-uint16_t calculateDividerForRate(float rate, uint16_t maintiming, uint16_t prescaler);
-int32_t calculateRateForDivider(uint16_t divider, uint16_t maintiming, uint16_t prescaler);
