ebb4af8368d14e13ca573891fafe4624f0acb3d3
PaulZiMAR <p.ziegler@imar-navigation.de>
2025-07-18 06:18:09 +0000

    b0036be6

--- src/ixcom_driver_lc/src/modules/utility.cpp
+++ src/ixcom_driver_lc/src/modules/utility.cpp
@@ -13,11 +13,15 @@ double gpsTimeStamp(const double& time, int32_t leap_seconds) {
     // int s = time.seconds() - 315964800 + leap_seconds;
     // int us = std::round(time.nanoseconds() / 1000);
 
-    uint32_t t_s = static_cast<uint32_t>(time);
-    uint32_t t_ns = (time - t_s) * 1e9;
-    uint32_t s = t_s - 315964800 + leap_seconds;
-    uint32_t us = std::round(t_ns / 1000);
-    return s + us * 1e-6;
+    // uint32_t t_s = static_cast<uint32_t>(time);
+    // uint32_t t_ns = (time - t_s) * 1e9;
+    // uint32_t s = t_s - 315964800 + leap_seconds;
+    // uint32_t us = std::round(t_ns / 1000);
+    // return s + us * 1e-6;
+
+    double s = time - 315964800.0 + leap_seconds; // GPS epoch starts at 1980-01-06
+    double tow = fmod(s, 604800.0); // only seconds of week remain
+    return tow;
 }
 
 uint16_t calculateDividerForRate(float rate, uint16_t maintiming, uint16_t prescaler) {
